4bfc4ba719144153ec78a314d6a7b59ff049b956
Alex <alex.thepaut33@gmail.com>
2019-02-12 14:24:49 +0100

    Ajout des pluggins pour compiler en jar avec toutes les dépendences*

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.Scanner;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("J'ai faim");
-        System.out.println(asciiArt);
+    	while(true){
+    		Scanner text = new Scanner(System.in);
+    		String asciiArt = FigletFont.convertOneLine(text.next());
+    		System.out.println(asciiArt);
+    	}
     }
 }
