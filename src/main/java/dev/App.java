289e0b2a999833755dd6be9319e47570d92fe3bd
Alex <alex.thepaut33@gmail.com>
2019-02-12 15:26:35 +0100

    Ajout du filtering et du fichier de propriété

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import com.github.lalyos.jfiglet.FigletFont;
@@ -12,9 +13,16 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArtTitre = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArtTitre);
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
     	while(true){
+    		String question = ResourceBundle.getBundle("application").getString("question");
+        	System.out.println(question);
     		Scanner text = new Scanner(System.in);
-    		String asciiArt = FigletFont.convertOneLine(text.next());
+    		String asciiArt = FigletFont.convertOneLine(text.nextLine());
     		System.out.println(asciiArt);
     	}
     }
